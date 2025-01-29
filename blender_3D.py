21e0d0eb7d3d2e6f7b3817af668e0491e8df605d
yami-no-senshi <dashimashimashi1028@gmail.com>
2025-01-29 11:08:50 +0900

    Add files via upload

--- blender_3D.py
+++ blender_3D.py
@@ -19,22 +19,6 @@ def delete_all():
   for item in bpy.data.materials:
     bpy.data.materials.remove(item)
 
-# 射影座標からリージョン座標へ変換する関数
-def viewport_transform(region, v):
-    wh = region.width / 2.0
-    hh = region.height / 2.0
-    print("v : ", v.x, v.y, v.z, v.w)
-
-    if v.w == 0:
-      a = wh
-      b = hh
-    else:
-      a = wh + wh * v.x / v.w
-      b = hh + hh * v.y / v.w
-
-    return Vector((a, b))
-
-
 # 指定したエリア、リージョン、スペースを取得する関数
 def get_region_and_space(area_type, region_type, space_type):
     region = None
@@ -60,8 +44,6 @@ def get_region_and_space(area_type, region_type, space_type):
             break
 
     return (area, region, space)
-
-
 
 def project_3d_point(camera: bpy.types.Object,
                      p: Vector,
@@ -113,8 +95,8 @@ def project_3d_point(camera: bpy.types.Object,
 
     return p2
 
-if __name__ == "__main__":
-  delete_all()
+
+def main():
 
   # 
   Target_Mesh_Name = 'Cube_Mesh'
@@ -135,23 +117,10 @@ if __name__ == "__main__":
   bpy.context.object.rotation_euler[0] = -4.5
   bpy.context.object.rotation_euler[1] = 0.26
   bpy.context.object.rotation_euler[2] = 1.6
-  #bpy.context.scene.camera = bpy.data.objects["Camera"]
-  #bpy.ops.view3d.camera_to_view()
 
   for area in bpy.context.screen.areas:
     if area.type == 'VIEW_3D':
         area.spaces[0].region_3d.view_perspective = 'CAMERA'
-
-#        area.spaces.active.region_3d.view_perspective = my_camera
-#        bpy.ops.view3d.camera_to_view()
-        #bpy.ops.view3d.object_as_camera()
-#        override = bpy.context.copy()
-#        override['area'] = area
-#        bpy.ops.view3d.object_as_camera(override)
-#        break
-
-  #以下のtypeのぶぶんには、TOP,BOTTOM,FRONT,BACK,RIGHT,LEFTのいずれかが入ります。
-  #bpy.ops.view3d.view_axis(type = 'TOP',align_active = True)
 
   #立方体を形成する頂点と面を定義する
   verts = [(0,0,0),(0,5,3),(5,5,0),(5,0,0),(0,0,5),(0,5,5),(5,5,5),(5,0,5)]
@@ -180,7 +149,6 @@ if __name__ == "__main__":
   # 3Dビューエリアのウィンドウリージョンのリージョンとスペースを取得
   (_, region, space) = get_region_and_space('VIEW_3D', 'WINDOW', 'VIEW_3D')
 
-  print(region, space)
   if space is not None:
     # 選択中の頂点のローカル座標を取得する
     obj = bpy.context.active_object
@@ -189,55 +157,23 @@ if __name__ == "__main__":
       Vector((v.co[0], v.co[1], v.co[2], 1.0))
       for v in bm.verts if v.select
     ]
-    print("vert_local", len(vert_local))
-#    for i in range(len(vert_local)):
-#      print(vert_local[i])
-
-    #bm.verts.ensure_lookup_table()
-    #for i in range(len( bm.verts)):
-    #  print("bm.verts", bm.verts[i].co)
-
-    bpy.ops.object.mode_set(mode="OBJECT", toggle=False)
     # ローカル座標からグローバル座標への変換
     vert_global = [obj.matrix_world @ v for v in vert_local]
-    # グローバル座標から射影座標への変換
-    projection_matrix = bpy.context.scene.camera.calc_matrix_camera(bpy.context.evaluated_depsgraph_get(), x=1920, y=1080)
-    print(projection_matrix)
-    print(space.region_3d.perspective_matrix)
-    vert_perspective = [
-#      space.region_3d.perspective_matrix @ v for v in vert_global
-       projection_matrix @ v for v in vert_global
-    ]
-    for v in vert_global:
-      print("global : ", v)
-    for v in vert_perspective:
-      print("perspective : ", v)
-    # 射影座標からリージョン座標への変換
-    vert_region = [viewport_transform(region, v) for v in vert_perspective]
-    # 座標を出力
-    for l, g, p, r in zip(vert_local, vert_global,
-                          vert_perspective, vert_region):
-      print("==========")
-      print("local: " + repr(l))
-      print("global: " + repr(g))
-      print("perspective: " + repr(p))
-      print("region: " + repr(r))
 
+    # 3次元座標のベクター形式に変換
+    vs = [
+      Vector((v[0], v[1], v[2]))
+      for v in vert_global
+    ]
 
 
   print("=====================")
   camera = bpy.data.objects['Camera']  # or bpy.context.active_object
   render = bpy.context.scene.render
 
-  Averts = [Vector((0,0,0)), \
-            Vector((0,5,3)), \
-            Vector((5,5,0)), \
-            Vector((5,0,0)), \
-            Vector((0,0,5)), \
-            Vector((0,5,5)), \
-            Vector((5,5,5)), \
-            Vector((5,0,5))]
-  for P in Averts:
+
+
+  for P in vs:
     print(P)
     #P = Vector((-0.002170146, 0.409979939, 0.162410125))
 
@@ -252,6 +188,16 @@ if __name__ == "__main__":
 
   print("Done.")
 
-  bpy.context.scene.render.image_settings.file_format = 'PNG'
-  bpy.ops.render.render()
-  bpy.data.images['Render Result'].save_render( filepath = 'hoge.png')
+#  bpy.context.scene.render.image_settings.file_format = 'PNG'
+#  bpy.ops.render.render()
+#  bpy.data.images['Render Result'].save_render( filepath = 'hoge.png')
+
+
+
+
+if __name__ == "__main__":
+  delete_all()
+
+  main()
+
+
